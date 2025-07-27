a0945b54ed75fb40d7e1c0d505bc8439730378af
deimos135-ai <ubnt5000@gmail.com>
2025-07-27 13:39:59 +0300

    Update main.py
    new main

--- app/main.py
+++ app/main.py
@@ -1,17 +1,16 @@
 import asyncio
 import os
-from aiogram import Bot, Dispatcher
+from aiogram import Bot
 from .scheduler import setup_scheduler
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
 
 async def main():
     await setup_scheduler(bot, CHAT_ID)
-    await dp.start_polling(bot)
+    await asyncio.Event().wait()  # Бот працює вічно, нічого не слухає
 
 if __name__ == "__main__":
     asyncio.run(main())
