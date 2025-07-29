bcbffbad53f580aa3b4e70d111ce1d3b13aabf87
deimos135-ai <ubnt5000@gmail.com>
2025-07-29 10:05:22 +0300

    Update main.py
    NEW BOT TOKEN

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from aiogram.enums.parse_mode import ParseMode
 from aiogram.filters import Command
 from .scheduler import setup_scheduler, get_random_message
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN_NEW")
 CHAT_ID = os.getenv("CHAT_ID")
 
 bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
